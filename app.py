d5a8b316572f5e906dffe34104bc7e4b37fcd56a
Tanjems <seguintristan012@gmail.com>
2026-03-23 22:14:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,13 @@ from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit, join_room, leave_room
 
 app = Flask(__name__)
-socketio = SocketIO(app, async_mode='eventlet', cors_allowed_origins='*')
+socketio = SocketIO(
+    app,
+    async_mode='eventlet',
+    cors_allowed_origins='*',
+    logger=True,
+    engineio_logger=True
+)
 
 rooms = {}
 
