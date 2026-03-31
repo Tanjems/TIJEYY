7d78277a877586787238771e8c6369c6398ee916
Tanjems <seguintristan012@gmail.com>
2026-03-31 17:51:55 +0800

    Update app.py

--- app.py
+++ app.py
@@ -187,3 +187,7 @@ def handle_disconnect():
             if not rooms[room]['players']:
                 del rooms[room]
             break
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    socketio.run(app, host='0.0.0.0', port=port, debug=False)
